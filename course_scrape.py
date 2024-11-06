04d53eca74a82d251daa222156d7dc833dd2fc30
cathduan <Duan.C063@gmail.com>
2024-11-06 12:27:33 -0600

    Create course_scrape.py
    Moved my local python file into the github. A working but not amazing course scraper for Spring 2025 courses.

---
+++ course_scrape.py
@@ -0,0 +1,28 @@
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+# Making a GET request
+r = requests.get('https://www.carleton.edu/catalog/current/search/?text=%20&term=25SP')
+
+if r.status_code == 200:
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    course_list = soup.find('ul', class_='courseSearchResults')
+
+    descriptions = []
+
+    for course_item in course_list.find_all('div', class_='courseDetailWrapper'): 
+         descriptions.append(course_item.get_text(strip=True))
+    
+    #CSV
+    with open('courses.csv', mode='w', newline='', encoding='utf-8') as file:
+         writer = csv.writer(file)
+
+         writer.writerow('Description')
+         for description in descriptions:
+             writer.writerow([description])
+    print("CSV file 'courses.csv' has been created successfully.")
+else:
+    print("Failed to retrieve the page")
