d674948cf0abfbcc5c4185656852b21aa7a46024
cathduan <Duan.C063@gmail.com>
2024-11-10 22:39:54 -0600

    created a test react file and edited server.js

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './carleton_logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>CS399: Serendipitous Exploration of the Carleton Curriculum </h1>
+        <h3>Cathy Duan, Charlie Ney, Markus Gunadi, Kai Weiner, Zoey La, Willow Gu</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-res.send('Hello, World!');
-});
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/homepage.html'));
+  });
 
 app.listen(PORT, () => {
 console.log("Server is running at http://localhost:${PORT}");
