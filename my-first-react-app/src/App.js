496aaca2c25482916bda63b95c7b7ce52b2464ac
cathduan <Duan.C063@gmail.com>
2024-11-12 23:57:17 -0600

    Connecting React to our csv.
    Moved a csv into the react folder and edited App.js to display csv data on the localhost.

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,9 +1,67 @@
 import logo from './carleton_logo.png';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Papa from 'papaparse';
+import Data from './courses2.csv';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  // parse CSV data -- code taken from WebStylePress youtube video
+  // const handleFileUpload = (e) => {
+  //   const file = e.target.files[0];
+  //   Papa.parse(file, {
+  //     header: true,
+  //     complete: (results) => {
+  //       setData(results.data);
+  //     },
+  //   });
+  // };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(Data);
+      const reader = response.body.getReader();
+      const result = await reader.read();
+      const decoder = new TextDecoder("utf-8");
+      const csvData = decoder.decode(result.value);
+      const parsedData = Papa.parse(csvData, {
+        header: true,
+        skipEmptyLines: true
+      }).data;
+      setData(parsedData);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
+      {/* <input type = "file" accept = ".csv" onChange = {handleFileUpload}/> */}
+      {data.length ? (
+        <table className = "table">
+          <thead>
+            <tr>
+              <th>Course Number</th>
+              <th>Course Title</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody> 
+            {data.map((row,index)=> (
+              //table row element for each row
+              <tr key= {index}>
+                <td>{row.Course_Number}</td>
+                <td>{row.Course_Title}</td>
+                <td>{row.Description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : null}
+       {/* //conditional rendering expression; if greater than 0, render the content */}
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>CS399: Serendipitous Exploration of the Carleton Curriculum </h1>
