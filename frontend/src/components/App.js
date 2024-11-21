535d8f1d984e390a0275e65d113cdf70a8f8115c
Zoey La <zoeyla24@gmail.com>
2024-11-21 04:13:09 -0600

    working front end (basic)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,55 +1,10 @@
-import logo from './carleton_logo.png';
+import logo from '../carleton_logo.png';
 import './App.css';
-import { useState, useEffect } from 'react';
-import Papa from 'papaparse';
-import Data from './courses2.csv';
 
 function App() {
 
-  // const [data, setData] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await fetch(Data);
-  //     const reader = response.body.getReader();
-  //     const result = await reader.read();
-  //     const decoder = new TextDecoder("utf-8");
-  //     const csvData = decoder.decode(result.value);
-  //     const parsedData = Papa.parse(csvData, {
-  //       header: true,
-  //       skipEmptyLines: true
-  //     }).data;
-  //     setData(parsedData);
-  //   };
-  //   fetchData();
-  // }, []);
-
   return (
     <div className="App">
-    
-      {/* {data.length ? (
-        <table className = "table">
-          <thead>
-            <tr>
-              <th>Course Number</th>
-              <th>Course Title</th>
-              <th>Description</th>
-            </tr>
-          </thead>
-          <tbody> 
-            {data.map((row,index)=> (
-              //table row element for each row
-              <tr key= {index}>
-                <td>{row.Course_Number}</td>
-                <td>{row.Course_Title}</td>
-                <td>{row.Description}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : null} */}
-       {/* //conditional rendering expression; if greater than 0, render the content */}
-
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
