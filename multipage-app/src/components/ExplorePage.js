b05ad0dff1bf7e4b7455dfd103d13b51b0648dc7
willow-gu <129335186+willow-gu@users.noreply.github.com>
2024-11-23 22:30:36 -0600

    multipage app (runs independent from main)

---
+++ multipage-app/src/components/ExplorePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExplorePage = () => {
+  return (
+    <div>
+      <h1>Explore Similar Courses</h1>
+    </div>
+  );
+};
+
+export default ExplorePage;
---
+++ multipage-app/src/components/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import carleton from '../images/carleton_logo.png';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="homepage-container">
+      <header className="homepage-header">
+        <img src={carleton} className="homepage-logo" alt="Carleton Logo" />
+        <h1>
+         Curriculum Exploration
+        </h1>
+        <Link to="/explore">
+          <button className="homepage-button">Start!</button>
+        </Link>
+        <br />
+        <Link to="/about" className="learn-more-link">Learn more about this website</Link>
+      </header>
+    </div>
+  );
+};
+
+export default HomePage;
