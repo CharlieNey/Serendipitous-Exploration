2a1f262df6736067ecdd3a7f12da0c4ea75f16f1
willow-gu <129335186+willow-gu@users.noreply.github.com>
2024-12-09 18:20:15 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/homepage';
+import HomePage from './components/HomePage';
 import ExplorePage from './components/ExplorePage';
 import AboutPage from './components/AboutPage';
 
