a7a503acd7fc0789170546316ec2036240784c1f
KaiRW <kai.r.weiner@gmail.com>
2025-01-27 12:43:40 -0600

    Code w/ current problems for Cathy

--- frontend/src/components/GraphPage.js
+++ frontend/src/components/GraphPage.js
@@ -8,16 +8,46 @@ import { SavedCoursesContext } from './SavedCoursesContext.js';
 import { SearchContext } from './SearchContext.js';
 import { GraphContext } from './GraphContext.js';
 
-function getConnectedNodes(links, node) {
-  var connectedNodes = []
+// // calculate this once per course catalog
+// // MAKE THIS RUN ONCE- NESTED OBJECT
+// // WANT O(1) BEHAVIOR FOR "IS THIS OBJECT CONNECTED"- MAKE AN OBJECT
+// // CONNECTIONS['CS 111'] RETURNS [CS 202, ART 111]
+// // go through links each one time
+// function getConnectedNodes(links, node) {
+//   var connectedNodes = []
+//   for (var i in links) {
+//     if (links[i].source.id === node) {
+//       connectedNodes.push(links[i].target.id)
+//     } else if (links[i].target.id === node){
+//       connectedNodes.push(links[i].source.id)
+//     }
+//   }
+//   return connectedNodes
+// }
+
+function getAllNodeConnections(links) {
+  var connections = {}
   for (var i in links) {
-    if (links[i].source.id === node) {
-      connectedNodes.push(links[i].target.id)
-    } else if (links[i].target.id === node){
-      connectedNodes.push(links[i].source.id)
+    var node1 = links[i].source
+    var node2 = links[i].target
+
+    if(node1 in connections) {
+      if(!connections[node1].includes(node2)){
+        connections[node1].push(node2)
+      }
+    } else {
+      connections[node1] = [node2]
+    }
+
+    if(node2 in connections) {
+      if(!connections[node2].includes(node1)){
+        connections[node2].push(node1)
+      }
+    } else {
+      connections[node2] = [node1]
     }
   }
-  return connectedNodes
+  return connections;
 }
 
 function getLinkOpacity(link, selectedNode) {
@@ -28,16 +58,20 @@ function getLinkOpacity(link, selectedNode) {
 }
 
 function getNodeOpacity(node, selectedNode, connectedNodes) {
-  if (selectedNode === "" || node === selectedNode || connectedNodes.includes(node)) {
+  if (selectedNode === "" || node === selectedNode || connectedNodes[selectedNode].includes(node)) {
     return 1;
   }
   return 0.5;
 }
 
 function getNodeColor(node, selectedNode, connectedNodes) {
+  if (selectedNode === "") {
+    return "pink";
+  }
+
   if (node === selectedNode) {
     return "red";
-  } else if (connectedNodes.includes(node)){
+  } else if (connectedNodes[selectedNode].includes(node)){
     return "green";
   }
   return "pink";
@@ -53,6 +87,8 @@ const GraphPage = () => {
   const {courseList, searchTerm, isLoading, setSearchTerm, fetchCourses} = useContext(SearchContext);
   const { selectedNode, nodes, links, setSelectedNode, fetchNodes, fetchLinks } = useContext(GraphContext);
   const [clicked, setClicked] = useState(false)
+
+  const[connectedNodes, setConnectedNodes] = useState([]);
   
   // Fetch values for state variables
   useEffect(() => {
@@ -70,10 +106,15 @@ const GraphPage = () => {
 useEffect(() => {
   if (nodes.length === 0 || links.length === 0) return;
 
+  setConnectedNodes(getAllNodeConnections(links))
+
   const svg = d3
     .select("#simulation-svg")
     .attr("width", width)
     .attr("height", height)
+    .call(d3.zoom().on("zoom", (event) => { // MOVE THIS INTO THE FIRST USEFFECT
+      svg.attr("transform", event.transform);
+    }));
   
   svg.append("g").attr("class", "links");
   svg.append("g").attr("class", "nodes");
@@ -90,70 +131,37 @@ useEffect(() => {
         .data(links)
         .join("line")
 
-      const nodeGroup = d3.select(".nodes")
-        .selectAll("g")
-        .data(nodes)
-        .join("g")
-        .attr("transform", (d) => `translate(${d.x},${d.y})`);
-
       linksGroup
         .attr("x1", (d) => d.source.x)
         .attr("y1", (d) => d.source.y)
         .attr("x2", (d) => d.target.x)
         .attr("y2", (d) => d.target.y)
-    });
+        .style("stroke-width", 2)
 
-    return () => {
-      simulation.stop();
-    };
-  }, [links, nodes]);
+      const nodeGroup = d3.select(".nodes")
+        .selectAll("g")
+        .data(nodes)
+        .join("g")
 
+      // Adding the text to the circles
+      nodeGroup
+        .selectAll("text")
+        .data((d) => [d])
+        .join("text")
+        .text((d) => d.id)
+        .attr("dy", 1)
 
+      nodeGroup
+        .selectAll("circle")
+        .data((d) => [d])
+        .join("circle")
+        .attr("transform", (d) => `translate(${d.x},${d.y})`)
+        .style("r", 5)
+        .style("stroke-width", 0.5)
+        .style("stroke", "black");
 
-  useEffect(() => {
-    const svg = d3
-      .select("#simulation-svg")
-      .call(d3.zoom().on("zoom", (event) => {
-        svg.attr("transform", event.transform);
-      }));
-
-    const linksGroup = d3.select(".links")
-    .selectAll("line")
-    .data(links)
-    .join("line")
-
-  linksGroup
-    .style("opacity", (d) => getLinkOpacity(d, selectedNode))
-    .style("stroke", (d) => color((d.score - 0.5) * 2)) // Change to min similarity score
-    .style("stroke-width", 2)
-
-    const nodeGroup = d3.select(".nodes")
-      .selectAll("g")
-      .data(nodes)
-      .join("g")
-
-    var connectedNodes = getConnectedNodes(links, selectedNode)
-    nodeGroup
-      .selectAll("circle")
-      .data((d) => [d])
-      .join("circle")
-      .style("r", 5)
-      .style("fill", (d) => getNodeColor(d.id, selectedNode, connectedNodes))
-      .style("opacity", (d) => getNodeOpacity(d.id, selectedNode, connectedNodes))
-      .style("stroke-width", 0.5)
-      .style("stroke", "black");
-
+      // ONLY NEED TO SET THIS IN ORIGNAL GRAPH USEFFECT- FUNCTION ISN'T CHANGING
       selectAll('circle')
-        .on('click', function (e, d) {
-          if (clicked === true && selectedNode === d.id) {
-            setSelectedNode("");
-            setClicked(false);
-          } else {
-            setSelectedNode(d.id);
-            setClicked(true);
-          }
-
-        })
         .on('mouseover', function (e, d) {
           if(!clicked){
             setSelectedNode(d.id);
@@ -164,14 +172,47 @@ useEffect(() => {
             setSelectedNode("");
           }
         })
+        .on('click', function (e, d) {
+          if (clicked === true && selectedNode === d.id) {
+            setClicked(false);
+            setSelectedNode("");
+            // console.log("help")
+          } else {
+            setClicked(true);
+            setSelectedNode(d.id);
+            // console.log("new")
+          }
+        });
 
-      // Adding the text to the circles
-      nodeGroup
-        .selectAll("text")
-        .data((d) => [d])
-        .join("text")
-        .text((d) => d.id)
-        .attr("dy", 1);
+        // .attr("font-size", "1000px")
+        // .style("stroke", "black")
+        // .style("opacity", 1)
+        // .style("stroke-width", 2);
+
+    });
+
+    return () => {
+      simulation.stop();
+      svg.selectAll(".links").remove();
+      svg.selectAll(".nodes").remove();
+    };
+  }, [links, nodes]);
+
+
+
+  useEffect(() => {
+    // DON'T NEED TO CREATE NEW LINES, JUST WANT TO CHANGE STYLE
+    d3.select(".links")
+      .selectAll("line") // MIGHT BE CREATING NEW LINES EACH TIME THIS IS RUN
+      .style("opacity", (d) => getLinkOpacity(d, selectedNode))
+      .style("stroke", (d) => color((d.score - 0.5) * 2)) // Change to min similarity score
+
+    // REDUNDANT IN SAME WAY AS LINKS CODE
+    d3.select(".nodes")
+      .selectAll("g")
+      .selectAll("circle") //GRAB ELEMENTS FROM SVG, FOR EVERYTHING IN THAT SELECTION: DO THIS
+      .style("fill", (d) => getNodeColor(d.id, selectedNode, connectedNodes)) //ANONYMOUS FUNCTION
+      .style("opacity", (d) => getNodeOpacity(d.id, selectedNode, connectedNodes));
 
       }, [selectedNode]);
 
