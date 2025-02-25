d67c0e70084a94da5e51a4ee2c29b1d97a971240
Zoey La <zoeyla24@gmail.com>
2025-02-25 03:20:02 -0600

    Update psqlConfig.js

--- backend/psqlConfig.js
+++ backend/psqlConfig.js
@@ -1,5 +1,5 @@
-const my_user = 'postgres'
-const my_database = 'postgres'
-const my_password = 'kowerdio'
+const my_user = 'zoeyla'
+const my_database = 'courses'
+const my_password = ''
 
 export {my_user, my_database, my_password};
