7991d84e7bf791c0754c15815c166cdbafd9b8b8
Zoey La <zoeyla24@gmail.com>
2025-02-27 18:36:37 -0600

    Update graphTable.sql

--- backend/graphTable.sql
+++ backend/graphTable.sql
@@ -3,18 +3,20 @@ DROP TABLE IF EXISTS Similarities;
 DROP TABLE IF EXISTS Connections; -- For people with the old Connections table-- 
 
 CREATE TABLE Similarities (
-  source text,
-  target text,
-  similarity_score float,
-  desc1 text,
-  desc2 text, 
-  highlight_words text
+  source TEXT,
+  target TEXT,
+  similarity_score FLOAT,
+  desc1 TEXT,
+  desc2 TEXT, 
+  keywords1 TEXT,
+  keywords2 TEXT,
+  highlight_words TEXT
 );
 
-\COPY similarities (source, target, similarity_score, desc1, desc2, highlight_words) FROM 'data/graph_data/current_graph_data.csv' DELIMITER  ',' CSV HEADER;
+\COPY Similarities (source, target, similarity_score, desc1, desc2, keywords1, keywords2, highlight_words) FROM 'data/graph_data/current_graph_data.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE Nodes (
-    id text
+    id TEXT
 );
 
 -- To get distinct nodes -- 
